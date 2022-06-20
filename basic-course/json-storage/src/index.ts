3e1f98a41bc83515b754d6b675d60897b1911b04
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-06-20 19:23:39 +0300

    Added TS

---
+++ basic-course/json-storage/src/index.ts
@@ -0,0 +1,90 @@
+import fs from 'fs/promises';
+import path from 'path';
+import express from 'express';
+
+const index = express();
+const { PORT = 3000 } = process.env;
+
+index.use(express.json());
+
+// --- save custom path and data ---
+index.post('/:customPath', async function (req, res) {
+	try {
+		const customPath: string = req.params.customPath;
+		const body = req.body;
+		const data = {
+			path: customPath,
+			message: body,
+		};
+
+		if (await getData(customPath)) {
+			return res.send({
+				message: 'Sorry but this path is used',
+			});
+		} else {
+			await writeData(data);
+			return res.send({
+				message: 'success',
+				path: `localhost:${PORT}/${customPath}`,
+			});
+		}
+	} catch (error) {
+		throw error;
+	}
+});
+
+// --- search custom path and send response data ---
+index.get('/:customPath', async function (req, res) {
+	try {
+		const customPath = req.params.customPath;
+		const data = await getData(customPath);
+
+		if (data) {
+			return res.send({ data: data.message });
+		} else {
+			return res.send({ data: `Sorry, but this path does not exist` });
+		}
+	} catch (error) {
+		throw error;
+	}
+});
+// --------work with files ------------
+const filePath = path.join(__dirname, 'db', 'userPaths.json');
+
+const getAllData = async () => {
+	try {
+		const data: string = await fs
+			.readFile(filePath)
+			.then((data) => {
+				return data.toString();
+			})
+			.catch((error) => error);
+		return JSON.parse(data);
+	} catch (error) {
+		throw error;
+	}
+};
+
+const writeData = async (data: { path: string; message: any }) => {
+	try {
+		const oldData: { path: string; message: any }[] = await getAllData();
+		const newData = JSON.stringify(oldData.concat(data));
+		await fs
+			.writeFile(filePath, newData)
+			.then((data) => data)
+			.catch((error) => error);
+
+		return '';
+	} catch (error) {
+		throw error;
+	}
+};
+
+const getData = async (customPath: string) => {
+	const allData: { path: string; message: any }[] = await getAllData();
+	return allData.find((el: { path: string }) => el.path === customPath);
+};
+
+index.listen(PORT, () => {
+	console.log(`Server srart on localhost:${PORT} ...`);
+});

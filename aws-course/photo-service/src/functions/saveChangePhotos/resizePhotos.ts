512b5b059275c18d23a8ecdc196e6acd701bb04b
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-10-19 23:22:50 +0300

    Create resizePhotos.ts

---
+++ aws-course/photo-service/src/functions/saveChangePhotos/resizePhotos.ts
@@ -0,0 +1,30 @@
+import AWS from 'aws-sdk';
+import sharp from 'sharp';
+
+const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
+
+const resizePhotos = async (buff: Buffer, key: string) => {
+  const s3 = new AWS.S3();
+  let path: string = '';
+
+  if (key.includes('Demo')) {
+    path = key.replace('Demo.jpeg', 'DemoResize.jpeg');
+  } else {
+    path = key.replace('.jpeg', 'Resize.jpeg');
+  }
+
+  const sharpBuffer = await sharp(buff).resize(150, 150).toBuffer();
+
+  const params = {
+    ACL: 'public-read',
+    Body: sharpBuffer,
+    ContentType: 'image/jpeg',
+    Bucket: BUCKET_NAME,
+    Key: path,
+  };
+  const dataPhoto = await s3.upload(params).promise();
+
+  return dataPhoto;
+};
+
+export default resizePhotos;

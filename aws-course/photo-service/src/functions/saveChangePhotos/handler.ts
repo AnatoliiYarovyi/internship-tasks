5397f238bb730fdce40fdab843fe859c0ef3725a
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-10-19 21:47:07 +0300

    Update handler.ts

--- aws-course/photo-service/src/functions/saveChangePhotos/handler.ts
+++ aws-course/photo-service/src/functions/saveChangePhotos/handler.ts
@@ -1,13 +1,8 @@
-import AWS from 'aws-sdk';
 import Boom from '@hapi/boom';
 import mysql from 'mysql2/promise';
-import sharp from 'sharp';
-import axios from 'axios';
 
-import getBufferImg from './getBufferImg';
 import updatePhotosLoaded from '../../repositories/updatePhotosLoaded';
-
-const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
+import addWatermarkToPhoto from './addWatermarkToPhoto';
 
 const handler = async (event: any) => {
   const connection = await mysql.createConnection(process.env.DATABASE_URL);
@@ -16,9 +11,11 @@ const handler = async (event: any) => {
   // console.log('\nimage path: ', event.Records[0].s3.object.key);
 
   const key: string = event.Records[0].s3.object.key;
+  if (key.includes('Demo')) {
+    console.log('Stoped function saveChangePhotos');
+    return;
+  }
   const imageLink = `https://photos-from-photo-service.s3.amazonaws.com/${key}`;
-  const logoLink =
-    'https://photos-from-photo-service.s3.amazonaws.com/watermarks/photoDrop.png';
   const arrKeyData = key.split('/');
   const permission = arrKeyData[0];
   const imageName = arrKeyData.slice(-1)[0].replace('.jpeg', '');
@@ -41,42 +38,9 @@ const handler = async (event: any) => {
       break;
 
     case 'photographers':
-      const s3 = new AWS.S3();
       await updatePhotosLoaded(connection, imageName);
-      try {
-        const bufferPhoto = await getBufferImg(imageLink);
-        const bufferLogo = await getBufferImg(logoLink);
-        // const getPhoto = await axios({
-        //   method: 'get',
-        //   url: imageLink,
-        //   responseType: 'arraybuffer',
-        // });
-        // const bufferPhoto = Buffer.from(getPhoto.data, 'base64');
-
-        // const getLogo = await axios({
-        //   method: 'get',
-        //   url: imageLink,
-        //   responseType: 'arraybuffer',
-        // });
-        // const bufferLogo = Buffer.from(getLogo.data, 'base64');
-
-        const sharpBuffer = await sharp(bufferPhoto)
-          .composite([{ input: bufferLogo }])
-          .toBuffer();
-
-        const keyDemo = key.replace('.jpeg', 'Demo.jpeg');
-        const params = {
-          Bucket: BUCKET_NAME,
-          Key: keyDemo,
-          Body: sharpBuffer,
-        };
-
-        s3.upload(params, function (err, data) {
-          console.log('\n*** err ***', err, '\n*** data ***', data);
-        }).promise();
-      } catch (error) {
-        console.log(error);
-      }
+      const dataDemoPhoto = await addWatermarkToPhoto(imageLink, key);
+      console.log('\n*** dataDemoPhoto ***', JSON.stringify(dataDemoPhoto));
 
       break;
 
@@ -86,8 +50,7 @@ const handler = async (event: any) => {
 
   await connection.end(err => {
     if (err) {
-      console.log('\n*** endConnect --> err: ***', err);
-      return err;
+      throw Boom.badImplementation(err);
     } else {
       console.log('\n*** connect --> END ***');
     }

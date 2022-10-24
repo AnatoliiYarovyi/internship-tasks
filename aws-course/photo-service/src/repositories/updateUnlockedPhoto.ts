9b58dac774418b41bbc40918af2a7f7d455409ae
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-10-25 00:58:07 +0300

    Update updateUnlockedPhoto.ts

--- aws-course/photo-service/src/repositories/updateUnlockedPhoto.ts
+++ aws-course/photo-service/src/repositories/updateUnlockedPhoto.ts
@@ -2,14 +2,14 @@ import mysql from 'mysql2/promise';
 
 const updateUnlockedPhoto = async (
   connection: mysql.Connection,
-  photoId: number,
+  albumId: number,
   nickName: string,
 ) => {
   const writeData =
-    'UPDATE `clients_photos` AS cp JOIN `clients` AS c ON cp.clientId = c.clientId SET cp.unlockedPhoto = 1 WHERE cp.photoId = ? AND c.phone = ?;';
+    'UPDATE `clients_photos` AS cp JOIN `clients` AS c ON cp.clientId = c.clientId SET cp.unlockedPhoto = 1 WHERE cp.albumId = ? AND c.phone = ?;';
 
   const [rows, fields] = await connection.execute(writeData, [
-    photoId,
+    albumId,
     nickName,
   ]);
   console.log('\n**** rows ***', rows, '\n**** fields ***', fields);

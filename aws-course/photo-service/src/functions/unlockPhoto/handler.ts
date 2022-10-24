165fa4b1a0cc8cb06e033b1adddde5086279b295
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-10-25 00:57:35 +0300

    Update handler.ts

--- aws-course/photo-service/src/functions/unlockPhoto/handler.ts
+++ aws-course/photo-service/src/functions/unlockPhoto/handler.ts
@@ -5,10 +5,10 @@ import updateUnlockedPhoto from '../../repositories/updateUnlockedPhoto';
 
 const handler = async (event: Event) => {
   const { connection } = event.body;
-  const { photoId } = event.queryStringParameters;
+  const { albumId } = event.queryStringParameters;
   const nickname = event.requestContext.authorizer.claims.nickname;
 
-  await updateUnlockedPhoto(connection, photoId, nickname);
+  await updateUnlockedPhoto(connection, albumId, nickname);
 
   return {
     status: 'success',

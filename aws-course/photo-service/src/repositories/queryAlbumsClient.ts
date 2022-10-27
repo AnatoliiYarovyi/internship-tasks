b964f457042ffa32d382067d44278029def6fc53
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-10-27 05:31:30 +0300

    Update queryAlbumsClient.ts

--- aws-course/photo-service/src/repositories/queryAlbumsClient.ts
+++ aws-course/photo-service/src/repositories/queryAlbumsClient.ts
@@ -4,7 +4,7 @@ const queryAlbumsClient = async (
   connection: mysql.Connection,
   phone: string,
 ) => {
-  const query = `SELECT a.albumId, a.albumName, a.location, a.specifiedTimestamp 
+  const query = `SELECT a.albumId, a.albumName, a.location, a.specifiedTimestamp, a.albumCoverLink
 FROM clients_photos AS cp
 JOIN clients AS c ON cp.clientId = c.clientId 
 JOIN photos AS p ON cp.photoId = p.photoId

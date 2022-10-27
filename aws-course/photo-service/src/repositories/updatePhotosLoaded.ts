39dfdc46fbfc3b393c0136afe0c3aae3bebd0f7a
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-10-27 05:31:58 +0300

    Update updatePhotosLoaded.ts

--- aws-course/photo-service/src/repositories/updatePhotosLoaded.ts
+++ aws-course/photo-service/src/repositories/updatePhotosLoaded.ts
@@ -2,12 +2,12 @@ import mysql from 'mysql2/promise';
 
 const updatePhotosLoaded = async (
   connection: mysql.Connection,
-  name: string,
+  photoId: number,
 ) => {
   const writeData =
-    'UPDATE `photos` SET photos.loaded = 1 WHERE photos.name = ?;';
+    'UPDATE `photos` SET photos.loaded = 1 WHERE photos.photoId = ?;';
 
-  const [rows, fields] = await connection.execute(writeData, [name]);
+  const [rows, fields] = await connection.execute(writeData, [photoId]);
   console.log('\n**** rows ***', rows, '\n**** fields ***', fields);
 };
 

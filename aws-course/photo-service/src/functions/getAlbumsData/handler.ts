21f2fee637863f877b65e498821373cb9c605c1c
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-12-01 19:43:24 +0200

    Update handler.ts

--- aws-course/photo-service/src/functions/getAlbumsData/handler.ts
+++ aws-course/photo-service/src/functions/getAlbumsData/handler.ts
@@ -5,13 +5,15 @@ import { middyfy } from '../../libs/lambda';
 import { Event } from '../../interface/interface';
 import queryAlbumsPhotographer from '../../repositories/queryAlbumsPhotographer';
 
+const TABLE_NAME = process.env.USERS_TABLE_NAME;
+
 const handler = async (event: Event) => {
   const dynamodb = new AWS.DynamoDB.DocumentClient();
   const nickname = event.requestContext.authorizer.claims.nickname;
   const { connection } = event.body;
 
   const currentUserDb = await dynamodb
-    .get({ TableName: 'UsersPhotoService', Key: { nickname: nickname } })
+    .get({ TableName: TABLE_NAME, Key: { nickname: nickname } })
     .promise()
     .catch(error => {
       throw Boom.badImplementation(error);

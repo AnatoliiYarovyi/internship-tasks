806306de14feb7a268b51b445ea80fc76d1a7b3b
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-12-01 19:42:41 +0200

    Update saveCurrentSelfie.ts

--- aws-course/photo-service/src/functions/changeSavePhotos/saveCurrentSelfie.ts
+++ aws-course/photo-service/src/functions/changeSavePhotos/saveCurrentSelfie.ts
@@ -1,10 +1,12 @@
 import AWS from 'aws-sdk';
 import Boom from '@hapi/boom';
 
+const TABLE_NAME = process.env.USERS_TABLE_NAME;
+
 const saveCurrentSelfie = async (nickname: string, selfieLink: string) => {
   const dynamodb = new AWS.DynamoDB.DocumentClient();
   const currentUserDb = await dynamodb
-    .get({ TableName: 'UsersPhotoService', Key: { nickname } })
+    .get({ TableName: TABLE_NAME, Key: { nickname } })
     .promise()
     .catch(error => {
       throw Boom.badImplementation(error);
@@ -13,7 +15,7 @@ const saveCurrentSelfie = async (nickname: string, selfieLink: string) => {
   currentUserDb.Item.selfieLink = selfieLink;
 
   await dynamodb
-    .put({ TableName: 'UsersPhotoService', Item: currentUserDb.Item })
+    .put({ TableName: TABLE_NAME, Item: currentUserDb.Item })
     .promise()
     .catch(error => {
       throw Boom.badImplementation(error);

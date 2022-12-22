7332559b8f7cb6d35329635cae93d333dc70f583
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-12-22 18:46:21 +0200

    Added to response userId from psql DB

--- aws-course/photo-service-drizzle/src/functions/getClientsData/handler.ts
+++ aws-course/photo-service-drizzle/src/functions/getClientsData/handler.ts
@@ -2,14 +2,17 @@ import AWS from 'aws-sdk';
 import Boom from '@hapi/boom';
 
 import { middyfy } from '../../libs/lambda';
+
 import { Event } from '../../interface/interface';
+import { Client } from '../../repositories/Client';
 
 const TABLE_NAME = process.env.USERS_TABLE_NAME;
 
 const handler = async (event: Event) => {
   const dynamodb = new AWS.DynamoDB.DocumentClient();
-
   const nickname = event.requestContext.authorizer.claims.nickname;
+  const { connection } = event.body;
+  const client = new Client(connection);
 
   const currentUserDb = await dynamodb
     .get({ TableName: TABLE_NAME, Key: { nickname: nickname } })
@@ -18,11 +21,13 @@ const handler = async (event: Event) => {
       throw Boom.badImplementation(error);
     });
 
+  const userId = await client.getCurrentClientId(nickname);
+
   return {
     status: 'success',
     message: 'getClientsData successful',
     data: {
-      id: currentUserDb.Item.id,
+      id: userId,
       nickname: currentUserDb.Item.nickname,
       fullName: currentUserDb.Item.fullName,
       email: currentUserDb.Item.email,
